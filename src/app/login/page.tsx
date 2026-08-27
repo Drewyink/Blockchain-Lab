38ce13cec1a4f4b1d6f81e8a7acf9b3827e8e6be
Drewyink <drew.yinka@echolinksolutions.com>
2026-08-27 15:23:11 -0500

    Delete src directory

--- src/app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import { AuthForm } from "@/components/auth/AuthForm";
-
-export default function LoginPage() {
-  return (
-    <div className="flex min-h-[calc(100vh-4rem)] items-center justify-center px-4 py-12">
-      <AuthForm mode="login" />
-    </div>
-  );
-}
