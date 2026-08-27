c226fc66c34393efa44e0bef02755fc6fd03aa72
Drewyink <drew.yinka@echolinksolutions.com>
2026-08-27 16:22:20 -0500

    Delete src directory

--- src/app/signup/page.tsx
+++
@@ -1,9 +0,0 @@
-import { AuthForm } from "../../components/auth/AuthForm";
-
-export default function SignupPage() {
-  return (
-    <div className="flex min-h-[calc(100vh-4rem)] items-center justify-center px-4 py-12">
-      <AuthForm mode="signup" />
-    </div>
-  );
-}
